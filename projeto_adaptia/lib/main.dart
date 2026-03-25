d262aed8da12517891f15041ed4bd5927375d9eb
JEFERSON DE LIMA MENDES <jeferson.mendes@ufrpe.br>
2026-03-25 00:05:47 -0300

    CONFIG DO .ENV

--- projeto_adaptia/lib/main.dart
+++ projeto_adaptia/lib/main.dart
@@ -9,11 +9,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: FirebaseOptions(
-      apiKey: dotenv.env['FIREBASE_API_KEY'] ?? '',
-      appId: dotenv.env['FIREBASE_APP_ID'] ?? '',
-      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID'] ?? '',
-      projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
-      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+      apiKey: dotenv.env['AIzaSyBqAIVUkXXaMqL50v75TTvYAihrxWTRVsE'] ?? '',
+      appId: dotenv.env['453007687926:android:b44fd8872469dc02b42cd7'] ?? '',
+      messagingSenderId: dotenv.env['453007687926'] ?? '',
+      projectId: dotenv.env['adaptia-3d7aa'] ?? '',
+      storageBucket: dotenv.env['adaptia-3d7aa.firebasestorage.app'] ?? '',
   ),);
   setupDependencies(); // registra tudo antes do app abrir
   runApp(const MyApp());
