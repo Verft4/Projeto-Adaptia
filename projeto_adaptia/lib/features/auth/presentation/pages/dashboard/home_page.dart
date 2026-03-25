399fdd63a5334ef2c16e014305306b6714e2f1ad
JEFERSON DE LIMA MENDES <jeferson.mendes@ufrpe.br>
2026-03-25 06:49:55 -0300

    ATUALIZANDO

---
+++ projeto_adaptia/lib/features/auth/presentation/pages/dashboard/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Dashboard Home',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16),
+          Text('Bem-vindo ao seu dashboard! 🚀'),
+        ],
+      ),
+    );
+  }
+}
